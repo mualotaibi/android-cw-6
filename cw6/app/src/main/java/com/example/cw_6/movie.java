aec416428dd3a8e2bd74f92538b05c1f0f0f141f
munera <muneraalotaibii@gmail.com>
2020-07-01 17:54:45 +0300

    done cw-6

---
+++ cw6/app/src/main/java/com/example/cw_6/movie.java
@@ -0,0 +1,9 @@
+package com.example.cw_6;
+
+public class movie {
+    private String title ;
+    private String mainActor ;
+    private Double movieRate;
+    private int pgRate ;
+    private String genre ;
+}
